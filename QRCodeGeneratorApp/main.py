c59d1d9a65113502fa44b74270019cc945e9d56d
altoshi1 <toshialgin@gmail.com>
2024-09-26 12:07:45 +0200

    mise à jour

--- QRCodeGeneratorApp/main.py
+++ QRCodeGeneratorApp/main.py
@@ -8,41 +8,46 @@ from PIL import ImageTk, Image
 class QRCodeGeneratorApp:
     def __init__(self, master):
         self.master = master
-        master.title("Générateur de QR Code")
+        master.title("Générateur de QR Code") #Titre de la fenêtre
         master.geometry("600x500")  # Taille de la fenêtre
 
+        #Texte invitant l'utilisateur à saisir une URL
         self.label = tk.Label(master, text="Veuillez entrer une adresse URL pour générer un QR Code :", font=("Helvetica", 12))
-        self.label.pack(pady=10)
-
-        self.entry = tk.Entry(master, font=("Helvetica", 12), width=40)
-        self.entry.pack(pady=5)
-
-        self.generate_button = tk.Button(master, text="Générer QR Code", font=("Helvetica", 12), command=self.generate_qr_code)
+        self.label.pack(pady=10) #Place le label avec un espacement vertical de 10 pixels
+        #Créer un champ ou l'utilisateur peut saisir une URL
+        self.entry = tk.Entry(master, font=("Helvetica", 12), width=40) # Font texte
+        self.entry.pack(pady=5) #Espacement vertical
+        #Créer un bouton permettant de générer le QR Code
+        self.generate_button = tk.Button(master, text="Générer QR Code", font=("Helvetica", 12), command=self.generate_qr_code) #Si le bouton est cliqué appel la fonction generate_qr_code
         self.generate_button.pack(pady=10)
 
-        self.image_label = tk.Label(master)
+        self.image_label = tk.Label(master) #Créer un label pour afficher l'image du QR Code
         self.image_label.pack()
 
+    #Cette méthode sera appelée lorsque l'utilisateur cliquera sur le bouton pour générer le QR Code.
+    #Définition de la methode generate_qr_code
     def generate_qr_code(self):
-        url = self.entry.get()
-        if url:
+        url = self.entry.get() # Récuperation de l'url entré par l'utilisateur
+        if url: # Si URL entré par l'utilisateur, alors ...
+            #Crée un objet QRCode avec des paramètres spécifiques :
             qr = qrcode.QRCode(version=1, error_correction=qrcode.constants.ERROR_CORRECT_L, box_size=10, border=4)
-            qr.add_data(url)
+            qr.add_data(url) # Ajoute l'URL au QR Code et ajuste la taille du QR Code pour s'adapter aux données
             qr.make(fit=True)
 
-            img = qr.make_image(fill_color="black", back_color="white")
+            img = qr.make_image(fill_color="black", back_color="white") # Crée une image du QR Code avec des couleurs spécifiques.
 
             img = img.resize((300, 300))  # Redimensionner l'image du QR code
-
+            #Affichage de l'image en un format compatible
             self.qr_image = ImageTk.PhotoImage(img)
             self.image_label.config(image=self.qr_image)
-        else:
-            messagebox.showwarning("Avertissement", "Veuillez entrer une adresse URL pour générer un QR Code.")
+        else: # Si pas de URL entré alors ...
+            messagebox.showwarning("Avertissement", "Veuillez entrer une adresse URL pour générer un QR Code.") # Boîte de dialogue
 
-def main():
-    root = tk.Tk()
-    app = QRCodeGeneratorApp(root)
-    root.mainloop()
+def main():  #Définition de la fonction main/ Execution du programme
+    root = tk.Tk() # Initialise Tkinter - root contiendra tous les éléments de l'interface graphique
+    app = QRCodeGeneratorApp(root) # Création de l'instance de la classe , en passant root comme argument cela déclenche le constructeur de la classe, qui configure l'interface Utilisateur
+    root.mainloop() # Boucle d'événement de tkinter permettant d'attendre les interactions de l'utilisateur clics, clavier etc...
 
-if __name__ == "__main__":
-    main()
+if __name__ == "__main__": # Permet de vérifier si ce script est exécuté en tant que programme principal ou non
+    main() # Si le script est exécuté directement __name__ prend la valeur __main__ ce qui signifie que la fonction main() sera appelée
+    #Si la condition précédente est vraie la dernière ligne exécute la fonction main(), demarrant ainsi l'application
--- QRCodeGeneratorApp/test_main.py
+++ QRCodeGeneratorApp/test_main.py
@@ -1,4 +1,4 @@
-# test_main.py
+"""# test_main.py
 import unittest
 from unittest.mock import patch, MagicMock
 import tkinter as tk
@@ -30,5 +30,45 @@ class TestQRCodeGeneratorApp(unittest.TestCase):
         # Vérifier que l'image a été configurée
         app.image_label.config.assert_called_once()
 
+if __name__ == '__main__':
+    unittest.main()
+"""
+
+
+# POUR INSTALLER DEPUIS TERMINAL LINUX -> apt install python3-pillow
+
+from unittest.mock import MagicMock, patch
+import unittest
+from main import QRCodeGeneratorApp
+
+
+class TestQRCodeURL(unittest.TestCase): # création d'une nouvelle classe de test qui hérite de unittest.testcase
+
+    def setUp(self): #configuration de l'environnement de test et de mock
+        self.master_mock = MagicMock() # création d'un mock pour simuler le fichier maître - main.py
+        self.app = QRCodeGeneratorApp(self.master_mock) # on insere dans master mock la class principale QRCodeGeneratorApp
+        self.app.entry = MagicMock() # remplace la zone de saisie par un mock pour simuler les interactions avec celle-ci
+
+    def test_valid_url(self): # déclaration de la méthode test_valid_url
+        self.app.entry.get.return_value = 'http://exemple.com' # simule le retour de l'entrée clavier de l'utilisateur pour retourner une url valide
+
+        #
+        with patch('PIL.ImageTk.PhotoImage') as mock_photo_image: # On utilise patch pour remplacer ImageTk.PhotoImage par mock_photo_image pour éviter d'exectuer le code réel et eviter les erreurs
+            self.app.generate_qr_code() #appel de la méthode qui génère le QR Code
+
+        self.assertEqual(self.app.entry.get(), 'http://exemple.com') #Vérifie que la valeur renvoyée par entry.get() est bien l'url que nous avons simulée
+        mock_photo_image.assert_called_once()  # vérifie que la méthode PhotoImage a été appelée une fois pendant le test
+
+    @patch('tkinter.messagebox.showwarning') # on remplace showwarning et on simule les avertissement
+    def test_empty_url(self, mock_warning): # Déclaration de la méthode URL Vide
+        self.app.entry.get.return_value = '' # Simule le retour clavier utilisateur pour qu'il soit vide
+
+        with patch('PIL.ImageTk.PhotoImage'):  #on re-simule PhotoImage pour eviter les erreurs
+            self.app.generate_qr_code() # On génère le Qr Code
+        #On vérifie que showwarning a été appelé une fois avec le message identique spécifié ci-dessous
+        mock_warning.assert_called_once_with("Avertissement",
+                                             "Veuillez entrer une adresse URL pour générer un QR Code.")
+
+#Execution du script
 if __name__ == '__main__':
     unittest.main()
